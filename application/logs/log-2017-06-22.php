13b16f5aa78f97659cf7f8624fac9c1db58e3970
Tanguy Mervant <tanguy.mervant@clepsydra.dev>
2020-11-17 19:24:33 +0100

    Clear project

--- application/logs/log-2017-06-22.php
+++
@@ -1,8 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-DEBUG - 2017-06-22 09:31:03 --> UTF-8 Support Enabled
-DEBUG - 2017-06-22 09:31:03 --> Global POST, GET and COOKIE data sanitized
-ERROR - 2017-06-22 09:31:03 --> 404 Page Not Found: /index
-DEBUG - 2017-06-22 09:31:16 --> UTF-8 Support Enabled
-DEBUG - 2017-06-22 09:31:16 --> Global POST, GET and COOKIE data sanitized
-ERROR - 2017-06-22 09:31:16 --> 404 Page Not Found: /index
